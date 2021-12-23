fc63ab06a9bc28368b1b23d2552fd2515109190f
Sadi_er59 <Sadi_er59@STHQ0118-08>
2021-12-23 12:14:43 +0400

    Add project files.

---
+++ TcpClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            var ip = IPAddress.Parse("10.1.18.4");
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    var writer = Task.Run(() =>
+                    {
+
+                        while (true)
+                        {
+                            var text = Console.ReadLine();
+                            var stream = client.GetStream();
+                            var bw = new BinaryWriter(stream);
+                            bw.Write(text);
+                        }
+                    });
+                    var reader = Task.Run(() =>
+                    {
+
+                        var stream = client.GetStream();
+                        var br = new BinaryReader(stream);
+                        Console.WriteLine($"From : {br.ReadString()}");
+
+                    });
+
+                    Task.WaitAll(writer, reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ TcpListener/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+
+    //var buffer = new byte[1024];
+    //int length = stream.Read(buffer, 0, 1024);
+    //var msg = Encoding.UTF8.GetString(buffer, 0, length);
+    class Program
+    {
+        static TcpListener listener = null;
+        static BinaryWriter bw = null;
+        static BinaryReader br = null;
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Parse("10.1.18.38");
+            var ep = new IPEndPoint(ip, 27001);
+            listener = new TcpListener(ep);
+            listener.Start();
+            Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                Task.Run(() =>
+                {
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+
+                    var stream = client.GetStream();
+                    br = new BinaryReader(stream);
+                    bw = new BinaryWriter(stream);
+
+                    while (true)
+                    {
+                        var msg = br.ReadString();  
+                        Console.WriteLine($"{client.Client.RemoteEndPoint} : {msg}");
+                    }
+                });
+
+            }
+
+        }
+    }
+}
